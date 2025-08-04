963770d4c3190c657fcd87a99f12b7c02b54a107
box3101 <box4101@nate.com>
2025-08-04 19:02:25 +0900

    Refactor project structure and update dependencies. Changed project name to @example/basics, added build configuration in astro.config.mjs, and removed Welcome component. Updated package.json and package-lock.json with new dependencies and scripts. Added new files to .gitignore for zip archives.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    // Example: Generate `page.html` instead of `page/index.html` during build.
+    format: 'file',
+    assets: '_custom',
+  },
+});
