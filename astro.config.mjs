aa06943405d868e2422e524581b2d0363adc8eb2
cy.lee <box4101@nate.com>
2025-08-04 23:59:27 +0900

    프로젝트 이름을 @example/astro-clean으로 변경하고, Astro 구성 파일 및 패키지 의존성을 업데이트했습니다. Prettier 설정을 수정하고, 새로운 페이지 레이아웃을 추가했습니다. 불필요한 SVG 파일을 삭제하고, SCSS 파일에서 import 문을 @use로 변경했습니다.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,39 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   build: {
-    // Example: Generate `page.html` instead of `page/index.html` during build.
     format: 'file',
-    assets: '_custom',
+    assets: 'assets',
+    assetsPrefix: './'
   },
+
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            const info = assetInfo.name.split('.');
+            const ext = info[info.length - 1];
+
+            if (ext === 'css') {
+              // 해시 제거하고 css 폴더에 배치
+              return `css/[name].[ext]`;
+            }
+
+            if (ext === 'js') {
+              return `js/[name].[ext]`;
+            }
+
+            return `[name].[ext]`;
+          },
+          chunkFileNames: 'js/[name].js',
+          entryFileNames: 'js/[name].js'
+        }
+      }
+    }
+  },
+
+  server: {
+    port: 3000
+  }
 });
