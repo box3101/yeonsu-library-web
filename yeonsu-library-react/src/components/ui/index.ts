4c9e93765d7d4a0e1629f3bdfbd9311a96975994
box3101 <box4101@nate.com>
2025-09-09 10:10:48 +0900

    refactor: adjust padding and bullet point sizes in list components

--- yeonsu-library-react/src/components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-// UI Components Export
-export { Button, buttonVariants, type ButtonProps } from './Button';
-export { Input, inputVariants, type InputProps } from './Input';
-export { Select, selectVariants, type SelectProps } from './Select';
